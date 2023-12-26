ec4a3771f76e93247ebeb00a90209be89834770b
flyingtadpole <archer.curry@northwestern.edu>
2023-12-26 14:39:46 -0600

    added downloadable resume

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,5 +1,7 @@
+import resumePdf from '../../Archer Curry Resume Summer 2023 BA2.pdf'
+
 export default function Resume() {
     return(
-        <h2>Resume!</h2>
+        <a href={resumePdf}>Download Resume</a>
     )
 }
