2a03b2439de0f01f714d0d9dc0562fe7b88bf427
flyingtadpole <archer.curry@northwestern.edu>
2023-12-30 09:40:56 -0600

    fixed imbedded pdf and download link

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,15 +1,21 @@
-import { Card, Container, Image } from 'react-bootstrap'
-import resumePdf from '../../Archer Curry Resume Summer 2023 BA2.pdf'
+import { Card } from 'react-bootstrap';
+
+import resumePdf from '../../Resume.pdf'
+
+import pic1 from '../../resumepic1.jpg';
+import pic2 from '../../resumepic2.jpg';
+import pic3 from '../../resumepic3.jpg';
 
 export default function Resume() {
-    return(
-        <Container style={{width: '100vw', height: 'auto'}}>
-            <object data={resumePdf} type="application/pdf" >
-            </object>
-        </Container>
-        // <Card>
-        //     <Card.Img src={resumePdf} />
-        //     <a href={resumePdf}>Download Resume</a>
-        // </Card>
-    )
+
+  return (
+    <>
+        <a href={resumePdf}>Download Resume</a>
+        <Card style={{ width:"80%" }}>
+            <Card.Img src={pic1} />
+            <Card.Img src={pic2} />
+            <Card.Img src={pic3} />
+        </Card>
+    </>
+  );
 }
