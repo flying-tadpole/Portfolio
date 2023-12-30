512b80693c3b99d068d957071d06460828e2acc3
flyingtadpole <archer.curry@northwestern.edu>
2023-12-30 09:55:54 -0600

    added link for deployed application. updated portfolio descriptions

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -8,32 +8,38 @@ export default function Portfolio() {
         {
             title: "SVG Logo Maker",
             repo: 'https://github.com/flying-tadpole/SVGLogoMaker',
-            image: 'https://raw.githubusercontent.com/flying-tadpole/SVGLogoMaker/main/assets/commandLineDisplay.png'
+            image: 'https://raw.githubusercontent.com/flying-tadpole/SVGLogoMaker/main/assets/commandLineDisplay.png',
+            description: 'Command line application for generating a simple SVG logo'
         },
         {
             title: "SQL Employee Tracker",
             repo: 'https://github.com/flying-tadpole/EmployeeTracker',
-            image: 'https://i.postimg.cc/K8h0tfB3/employeetracker.png'
+            image: 'https://i.postimg.cc/K8h0tfB3/employeetracker.png',
+            description: 'Simple employee information tracker. CRUD capabilities'
         },
         {
             title: "The Escape Room",
             repo: 'https://github.com/flying-tadpole/The-Escape-Room',
-            image: 'https://i.postimg.cc/R0MGD0PM/escaperoom.png'
+            image: 'https://i.postimg.cc/R0MGD0PM/escaperoom.png',
+            description: 'Group project. Text based adventure game.'
         },
         {
             title: "NoSQL Social Network API",
             repo: 'https://github.com/flying-tadpole/NoSQL---Social-Network-API',
-            image: 'https://i.postimg.cc/y8hvjRyQ/NoSQL.png'
+            image: 'https://i.postimg.cc/y8hvjRyQ/NoSQL.png',
+            description: 'NoSQL based social media application. Backend only.'
         },
         {
             title: "Javascript Quiz",
             repo: 'https://github.com/flying-tadpole/JavascriptQuiz',
-            image: 'https://i.postimg.cc/gkZMwQXP/javascriptquiz.png'
+            image: 'https://i.postimg.cc/gkZMwQXP/javascriptquiz.png',
+            description: 'Interactive quiz.'
         },
         {
             title: "Art Block Breaker",
             repo: 'https://github.com/flying-tadpole/ArtBlockBreaker',
-            image: 'https://i.postimg.cc/DzB5kKD1/artblockbreaker.png'
+            image: 'https://i.postimg.cc/DzB5kKD1/artblockbreaker.png',
+            description: 'Group project. Uses Art Institute API. Generates word cloud based on favorited images.'
         }
     ]
 
